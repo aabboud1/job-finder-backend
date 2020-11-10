9578753b52c331fcd9a32e1cab319f7d2b0d6628
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-11-10 17:29:52 -0500

    Updated user model associations so that govt user can see all jobs, a company user can see all bids, and a company user can see all the jobs they have recieved a contract for.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :posted_jobs, :foreign_key => 'govt_id', :class_name => 'Job'
-    has_many :job_bids, :foreign_key => 'company_id', :class_name => 'Job'
-    has_many :bids, through: :jobs
+    has_many :job_bids, :foreign_key => 'company_id', :class_name => 'Bid'
+    has_many :received_jobs, :foreign_key => 'company_id', :class_name => 'Job'
     has_secure_password
 end
